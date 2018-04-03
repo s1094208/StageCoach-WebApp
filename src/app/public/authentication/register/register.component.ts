37838e491af36796f3ba2cdcfdacba4ff2356c94
Anna <s1094208@student.hsleiden.nl>
2018-04-03 14:44:50 +0200

    Changed the routing in the login html page. Added a register page.

--- src/app/public/authentication/register/register.component.ts
+++ src/app/public/authentication/register/register.component.ts
@@ -12,4 +12,23 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  onMail(mail) {
+    if (event['data'] == '@' && ((mail.value.match(/@/g) || []).length == 1)){
+      if (mail.value.includes("s") && mail.value.replace(/[^0-9]/g,"").length == 7){
+        console.log("Dit is een studenten mail!");
+        mail.value = mail.value + "student.hsleiden.nl";
+      } else {
+        console.log("Dit is geen studenten mail!");
+        mail.value = mail.value + "hsleiden.nl";
+      }
+    }
+  }
+
+  onRegister(firstpass, secondpass) {
+    if (firstpass.value != secondpass.value) {
+      //firstpass.borderColor
+      alert("The entered passwords are not the same!");
+    }
+  }
+
 }
