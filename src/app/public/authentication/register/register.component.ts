804a538b6a457774396ec5ec760dd81a8d79647e
Anna <s1094208@student.hsleiden.nl>
2018-04-05 09:27:07 +0200

    Changing to reactive forms for registration

--- src/app/public/authentication/register/register.component.ts
+++ src/app/public/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  mail = new FormControl();
 
   constructor() { }
 
@@ -26,7 +28,6 @@ export class RegisterComponent implements OnInit {
 
   onRegister(firstpass, secondpass) {
     if (firstpass.value != secondpass.value) {
-      //firstpass.borderColor
       alert("The entered passwords are not the same!");
     }
   }
